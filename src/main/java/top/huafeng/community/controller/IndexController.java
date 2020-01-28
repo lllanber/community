44842ff20c552323cb736e54ec59d2187531f510
huafeng <1246801578@qq.com>
2020-01-28 17:51:22 +0800

    add index and boostrap

---
+++ src/main/java/top/huafeng/community/controller/IndexController.java
@@ -0,0 +1,12 @@
+package top.huafeng.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String Index(){
+        return "index";
+    }
+}
